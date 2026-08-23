677edc21b79b761343a06eb2eaa827e3ff748bb4
Cesar-creator-coder <albertsilverios@gmail.com>
2026-08-23 00:00:59 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = "r1-marketplace-v1";
+const CACHE_NAME = "r1-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
   );
   self.skipWaiting();
 });
